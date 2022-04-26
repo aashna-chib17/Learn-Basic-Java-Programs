e1222949cfc84f146d1345265d1b2a54f48ff620
Aashna Chib <aashnachib17@gmail.com>
2022-04-26 15:47:41 +0530

    Create Factorial(Basic Method).java

---
+++ Factorial(Basic Method).java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main (String[] args){
+        int n=4;
+        int ans=1;
+        for(int i=1;i<=n;i++){
+            ans = ans*i;
+        }
+        System.out.println("Factorial of "+n+" is "+ans);
+    }
+}
